a69dbd627b904c263e737b067647567228659d9d
AdrianAntochi <adrian.constantin.antochi@gmail.com>
2014-11-19 13:16:06 +0200

    Added some libraries and renamed some classes

---
+++ src/org/openstreetmap/josm/plugins/dxfimport/Settings.java
@@ -0,0 +1,30 @@
+package org.openstreetmap.josm.plugins.dxfimport;
+
+import org.openstreetmap.josm.Main;
+
+public class Settings {
+
+    public static void setScaleNumerator(double value) {
+        Main.pref.putDouble("importdxf_scalenum", value);
+    }
+    public static void setScaleDivisor(double value) {
+        if (value == 0)
+            throw new IllegalArgumentException("Scale divisor cannot be 0");
+        Main.pref.putDouble("importdxf_scalediv", value);
+    }
+    public static void setCurveSteps(long value) {
+        if (value < 1)
+            throw new IllegalArgumentException("Curve steps cannot less than 1");
+        Main.pref.putLong("importdxf_curvesteps", value);
+    }
+    
+    public static double getScaleNumerator() {
+        return Main.pref.getDouble("importdxf_scalenum", 1);
+    }
+    public static double getScaleDivisor() {
+        return Main.pref.getDouble("importdxf_scalediv", 1);
+    }
+    public static double getCurveSteps() {
+        return Main.pref.getDouble("importdxf_curvesteps", 15);
+    }
+}
