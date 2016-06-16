e3903c4ac27cc245d05332d35144098f3c317819
Vincent Privat <vincent.privat@gmail.com>
2016-06-17 00:08:31 +0200

    use new API

--- src/org/openstreetmap/josm/plugins/dxfimport/DxfImportPlugin.java
+++ src/org/openstreetmap/josm/plugins/dxfimport/DxfImportPlugin.java
@@ -8,25 +8,6 @@ public class DxfImportPlugin extends Plugin {
 
 	public DxfImportPlugin(PluginInformation info) {
 		super(info);
-//		enableLogFile();
-		ExtensionFileFilter.importers.add(new DxfImporter()); // adding the file importer to josm file filter
+		ExtensionFileFilter.addImporter(new DxfImporter()); // adding the file importer to josm file filter
 	}
-
-/*	private void enableLogFile() {
-		String logFile = "D:\\logFile.txt";
-		try {
-			if (Files.exists(Paths.get(logFile))) {
-				Files.delete(Paths.get(logFile));
-			}
-			Files.createFile(Paths.get(logFile));
-			PrintStream stream = new PrintStream(new File(logFile));
-			System.setOut(stream);
-			System.setErr(stream);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}*/
-
 }
